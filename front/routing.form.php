6de62e32264aa83c6916aa4e668903b01b881b12
USman Khalid <engr.usman8855@gmail.com>
2026-07-31 09:35:14 +0500

    routing form update

--- front/routing.form.php
+++ front/routing.form.php
@@ -11,6 +11,38 @@ Html::header(
     'orientworkflow'
 );
 
+global $DB;
+
+if (isset($_POST['save'])) {
+
+    $DB->insert(
+        'glpi_plugin_orientworkflow_routes',
+        [
+            'branch'            => $_POST['branch'],
+            'service'           => $_POST['service'],
+            'category'          => $_POST['category'],
+            'group_id'          => (int)$_POST['group_id'],
+            'technician_id'     => !empty($_POST['technician_id'])
+                                    ? (int)$_POST['technician_id']
+                                    : null,
+            'itilcategories_id' => !empty($_POST['itilcategories_id'])
+                                    ? (int)$_POST['itilcategories_id']
+                                    : null,
+            'priority'          => !empty($_POST['priority'])
+                                    ? (int)$_POST['priority']
+                                    : null,
+            'sla_id'            => !empty($_POST['sla_id'])
+                                    ? (int)$_POST['sla_id']
+                                    : null,
+            'is_active'         => (int)$_POST['is_active']
+        ]
+    );
+
+    Session::addMessageAfterRedirect("Routing Rule Saved");
+
+    Html::redirect($_SERVER['PHP_SELF']);
+}
+
 echo "<div class='center'>";
 
 echo "<form method='post' action=''>";
@@ -188,7 +220,10 @@ echo "<td colspan='2' class='center'>";
 
 echo Html::submit(
     __('Save')
-);
+    [
+        'name' => 'save'
+    ]
+);1
 
 echo "</td>";
 echo "</tr>";
